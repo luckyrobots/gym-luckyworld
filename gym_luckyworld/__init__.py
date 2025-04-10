79843506019468050d79166de5f1b1bbdb269d5d
Ethan Clark <eclark715@gmail.com>
2025-04-10 10:18:54 -0700

    add navigation task with stretch

--- gym_luckyworld/__init__.py
+++ gym_luckyworld/__init__.py
@@ -11,3 +11,15 @@ register(
         "obs_type": "pixels_agent_pos",
     },
 )
+
+register(
+    id="gym_luckyworld/LuckyWorld-Navigation-v0",
+    entry_point="gym_luckyworld.env:LuckyWorldEnv",
+    max_episode_steps=300,
+    nondeterministic=True,
+    kwargs={
+        "task": "navigation",
+        "robot_type": "stretch_v1",
+        "obs_type": "pixels_agent_pos",
+    },
+)
